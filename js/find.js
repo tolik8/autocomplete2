359f1c1dd33822535437d08550bdb811399eaea9
tolik8 <tolik8@gmail.com>
2024-04-23 23:24:42 +0300

    continue

--- js/find.js
+++ js/find.js
@@ -10,28 +10,39 @@ $(document).ready(function() {
             onSelect: function (suggestion) {
                 //console.log(suggestion.data + ' ' + suggestion.value);
                 
+                // Створення нового елемента div
+                var nameBlock = $('<div class="name-block"></div>');
+
                 // Створення нового елемента input
-                var newInput = $('<input type="text" name="pib[]" class="dynamic-input" value="' + suggestion.value + '">');
+                var newInput = $('<input type="text" name="pib[' + suggestion.data + ']" class="dynamic-input" value="' + suggestion.value + '" readonly>');
                 
                 // Створення div для видалення інпута
-                var deleteButton = $('<div class="delete-button">X</div>');
+                //var deleteButton = $('<span class="delete-button">X</span>');
+                var deleteButton = $('<img class="delete-button" src="/img/del.png">');
                 
-                // Додавання інпута та кнопки в контейнер (наприклад, body)
-                $('#input-container').append(newInput);
-                newInput.after(deleteButton);
+                // Додавання інпута та кнопки в блок імені
+                nameBlock.append(newInput); // Додати інпут до блоку імені
+                nameBlock.append(deleteButton); // Додати кнопку видалення до блоку імені
+
+                // Додати блок імені з усіма елементами в контейнер
+                $('#input-container').append(nameBlock);
                 
                 // Очищення поля, в якому ведеться пошук
                 $('#find').val('')
                 
-                // Обробник події для видалення
+                // Обробник події для видалення новостворених елементів
                 deleteButton.click(function() {
-                    newInput.remove();
-                    deleteButton.remove();
+                    nameBlock.remove();
                 });
             }
         });
     }
 
     DevBridgeAutocompleteInit();
+    
+    // Обробник події для видалення
+    $('.delete-button').click(function() {
+        $(this).parent().remove();
+    });
 
 });
